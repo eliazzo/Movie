145279db5dcbe1ec0b3eb89b8d02be2e5becbbd9
eliazzo <bethazz@hotmail.co.uk>
2022-11-01 17:04:22 +0000

    css changes

--- movie.js
+++ movie.js
@@ -37,10 +37,48 @@ let movieData = {
   }
 
 
+  /* let movieNameArray = Object.keys(movieData)
+  console.log(movieNameArray)
+  movieNameArray.forEach(function(movieName){
+    console.log(movieName)
+  }) // Prints a list of the movie Names */
+
+
+
+
+
+ 
+  //Displaying movie data on webpage
+
+  const movieDataArrayList = document.getElementById("movie-info")
+
+  let movieDataArray = Object.keys(movieData)
+  console.log(movieDataArray)
+
+  for (let i in movieData){movieData[i].plot}
+
+  /*for (let i = 0; i < movieDataArray.length; i++){
+    const listData = document.createElement("li")
+    listData.textContent = movieDataArray [i]
+    movieDataArrayList.appendChild(listData)*/
+  
+
+
+  let movieDataArrayPrint = movieDataArray.forEach(function(printMovieInfo){
+    console.log(printMovieInfo)
+  }) //Prints the data of each movie individually (separated by movie)
+  
+  
+    
+    
+  
+    
+  
+
   //Transferring object data (movie names) to html list
   
   const movieNameList = document.getElementById("movie-name-list")
-  //console.log(movieNameList)
+  console.log(movieNameList)
   const movieNames = Object.keys(movieData)
 
 
@@ -49,6 +87,15 @@ let movieData = {
     listItem.textContent = movieNames[i]
     movieNameList.appendChild(listItem)
   }
+
+
+
+  /*for (let i = 0; i < movieInfo.length; i++){
+    const listItem = document.createElement("li")
+    listItem.textContent = movieInfo[i]
+    movieInfoList.appendChild(listItem)
+  } */
+
 
   //Transfering object data (movie cast) to html list  **************** working but I don't know how to create this using Object. ***************************
 
@@ -70,46 +117,47 @@ let movieData = {
       document.getElementById('darjeeling-year').innerText = movieData["The Darjeeling Limited"].year
       })
       
-          const darjeelingMouseover = document.getElementById("darjeeling-year")
+         /* const darjeelingMouseover = document.getElementById("darjeeling-year")
               darjeelingMouseover.addEventListener("mouseover", function(){
                 document.getElementById("darjeeling-year").innerText = movieData["The Darjeeling Limited"].rating
-              })
+              }) */
 
   const RoyalButton = document.getElementById('royal-year')
       RoyalButton.addEventListener("click", function(){
         document.getElementById('royal-year').innerText = movieData["The Royal Tenenbaums"].year
       })
 
-          const royalMouseover= document.getElementById('royal-year')
+        /*  const royalMouseover= document.getElementById('royal-year')
               royalMouseover.addEventListener("mouseover", function(){
                 document.getElementById('royal-year').innerText = movieData["The Royal Tenenbaums"].rating
-              })
+              }) */
 
   const foxButton = document.getElementById('fox-year')
       foxButton.addEventListener("click", function(){
         document.getElementById('fox-year').innerText = movieData["Fantastic Mr. Fox"].year
       })
 
-          const foxMouseover = document.getElementById('fox-year')
+        /*  const foxMouseover = document.getElementById('fox-year')
           foxMouseover.addEventListener("mouseover", function(){
               document.getElementById('fox-year').innerText = movieData["Fantastic Mr. Fox"].rating
-           })
+           }) */
   
   const budapestButton = document.getElementById('budapest-year')
       budapestButton.addEventListener("click", function(){
         document.getElementById('budapest-year').innerText = movieData["The Grand Budapest Hotel"].year
       })
 
-           const budapestMouseover = document.getElementById('budapest-year')
+        /*   const budapestMouseover = document.getElementById('budapest-year')
            budapestMouseover.addEventListener("mouseover", function(){
                 document.getElementById('budapest-year').innerText = movieData["The Grand Budapest Hotel"].rating
-          })
+          }) */
   
 
 
 
 
-/*Separating the Royal Tenenbaums into a new object
+//Separating the Royal Tenenbaums into a new object
+
   const theRoyalTenenbaums = {
     plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
       rating: 7.6,
@@ -123,20 +171,20 @@ let movieData = {
   document.getElementById("demo3").innerHTML = Object.keys(theRoyalTenenbaums)
 
   document.getElementById("demo4").innerHTML = Object.entries(theRoyalTenenbaums)
-  console.log(document.getElementById("demo4")) */
+  
 
 
 
 
 
-// Working again with the entire movieData object - plot
+// Working with the movieData object - plot text under image
 
     document.getElementById("darjeeling-plot").innerHTML = movieData["The Darjeeling Limited"].plot
-    //console.log(document.getElementById("darjeeling-plot").innerHTML)
-
     document.getElementById("royal-plot").innerHTML = movieData["The Royal Tenenbaums"].plot
     document.getElementById("fox-plot").innerHTML = movieData["Fantastic Mr. Fox"].plot
     document.getElementById("budapest-plot").innerHTML = movieData["The Grand Budapest Hotel"].plot
+
+    //
 
 
 /* Form 
@@ -168,33 +216,29 @@ let movies = []
 
   
   
-//List by rating
+//List of ratings
   
   document.getElementById("darjeeling-rating").innerHTML = movieData["The Darjeeling Limited"].rating
   document.getElementById("royal-rating").innerHTML = movieData["The Royal Tenenbaums"].rating
   document.getElementById("fantastic-rating").innerHTML = movieData["Fantastic Mr. Fox"].rating
   document.getElementById("budapest-rating").innerHTML = movieData["The Grand Budapest Hotel"].rating
 
-/* Mouse over image to reveal plot NOT WORKING *************************
-  const revealPlot = document.getElementById("Royal").src
-  revealPlot.addEventListener('click', function(){
-    document.getElementById("Royal").src.innerText = "hello"
-  }) */
-
-
-
-
+//END
 
 //Wes Anderson image change on mouse over
 
 function setNewImage(){
-  document.getElementById("Darjeeling").src = "Wes Anderson.jpg"
+  document.getElementById("Darjeeling").src = "darjeeling cast.jpg"
    }
 
 function setOldImage(){ 
    document.getElementById("Darjeeling").src = "Darjeeling limited.jpg"
 }
 
+/*function setNewImage(){
+  document.getElementById("Royal").src = "Royal tenenbaums cast.jpg"
+}*/
+//END
 
 
 
