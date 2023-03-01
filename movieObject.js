5dcc17e66f74ac2d09d2d798cb047b77ffb29106
eliazzo <bethazz@hotmail.co.uk>
2023-03-01 14:47:58 +0000

    Refactor rating poster UI to DOM

---
+++ movieObject.js
@@ -0,0 +1,41 @@
+// MovieData object
+
+let movieData = { // object made up of 4 keys and 4 values
+    "The Darjeeling Limited": { // key (mostly a string)
+      plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+      cast: ["Jason Schwartzman", " Owen Wilson", " Adrien Brody"],
+      runtime: 151,
+      rating: 7.2,
+      userRating: "",
+      year: 2007, // lines 5-10 value of "The Darjeeling Limited" key of movieData object but also an object itself made up of 6 key value pairs
+    },
+    "The Royal Tenenbaums": {
+      plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+      rating: 7.6,
+      year: 2001,
+      cast: ["Gene Hackman", " Gwnyeth Paltrow", " Anjelica Huston"],
+      runtime: 170,
+      userRating: "",
+    },
+    "Fantastic Mr. Fox": {
+      year: 2009,
+      plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+      cast: [
+        "George Clooney",
+        " Meryl Streep",
+        " Bill Murray",
+        " Jason Schwartzman",
+      ],
+      runtime: 147,
+      rating: 7.9,
+      userRating: "",
+    },
+    "The Grand Budapest Hotel": {
+      rating: 8.1,
+      runtime: 159,
+      year: 2014,
+      plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+      cast: ["Ralph Fiennes", " F. Murray Abraham", " Mathieu Amalric"],
+      userRating: "",
+    },
+  }
